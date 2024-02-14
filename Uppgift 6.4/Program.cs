4b28a9b9b4fe49547325e0d91dcdbd34813c31e3
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-14 13:08:26 +0100

    Add project files.

---
+++ Uppgift 6.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv basen till din potens:");
+            double bas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv exponenten till din potens:");
+            int exponent = int.Parse(Console.ReadLine());
+            Console.WriteLine("Svaret är = " + Metod(bas, exponent));
+        }
+
+        static double Metod(double bas, int exponent)
+        {
+            double svar = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                svar *= bas;
+            }
+            return svar;
+        }
+    }
+}
